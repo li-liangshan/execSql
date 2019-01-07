1fcba63f0f0222f85065aebd093064b874e43d4b
li-liangshan <liliangshanlls@gmail.com>
2019-01-07 13:34:24 +0800

    增加会话变量定义和修改表名SQL

--- src/sql/create.sql
+++ src/sql/create.sql
@@ -1,3 +1,7 @@
+--数据库
+CREATE DATABASE IF NOT EXISTS `db`;
+USE `db`;
+
 -- 表定义
 CREATE TABLE IF NOT EXISTS `daily_hit_counter`(
   `day` date NOT NULL COMMENT '时间',
@@ -7,3 +11,16 @@ CREATE TABLE IF NOT EXISTS `daily_hit_counter`(
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT '计数表';
 
 CREATE TABLE `customer_dev` LIKE `customer`; -- 从现有表创建新表
+
+-- 表改名 -下面最好都要走一遍
+RENAME TABLE `user_detail_1` TO `user_detail`;
+SET NAMES 'utf8';
+SHOW TABLE STATUS LIKE 'user_detail';
+SHOW CREATE TABLE `user_detail`;
+SET NAMES 'utf8mb4';
+SELECT COUNT(1) FROM `user_detail`;
+
+--
+
+
+
